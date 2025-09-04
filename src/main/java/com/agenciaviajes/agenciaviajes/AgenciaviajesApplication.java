71f856307a716b8430a6d0b4443deff86bb7b382
Jose Iban <ibandorta@gmail.com>
2025-09-04 08:56:24 +0200

    first commit

---
+++ src/main/java/com/agenciaviajes/agenciaviajes/AgenciaviajesApplication.java
@@ -0,0 +1,13 @@
+package com.agenciaviajes.agenciaviajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaviajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaviajesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agenciaviajes/agenciaviajes/AgenciaviajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenciaviajes.agenciaviajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaviajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
